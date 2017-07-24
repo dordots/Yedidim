091ef296c183834737ae63962a0299028780a3ff
Gal Halali <gahal1599@gmail.com>
2017-07-24 21:18:52 +0300

    Fixing login activity

--- app/src/main/java/com/startach/yedidim/LoginActivity.java
+++ app/src/main/java/com/startach/yedidim/LoginActivity.java
@@ -8,7 +8,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD:app/src/main/java/com/startach/yedidim/LoginActivity.java
         setContentView(R.layout.activity_login);
 
         if (checkIfFirstLogin()) {
@@ -18,9 +17,5 @@ public class LoginActivity extends AppCompatActivity {
 
     private boolean checkIfFirstLogin() {
         return true;
-=======
-        setContentView(R.layout.activity_main);
-//        asd
->>>>>>> acb11fe11fbd39c41200da7866de71d381ae7d49:app/src/main/java/com/startach/yedidim/MainActivity.java
     }
 }
