71b071ad2d7f428b5885206d3e5210aeafb9dbd4
mpeled <m_peled@yahoo.com>
2017-08-17 22:04:55 +0300

    Add MVP Architure

---
+++ app/src/main/java/com/startach/yedidim/Model/User.java
@@ -0,0 +1,20 @@
+package com.startach.yedidim.Model;
+
+/**
+ * Created by user on 17-Aug-17.
+ */
+
+public class User {
+    private int _id;
+    private String _phoneNum;
+    private String _name;
+
+    public int get_id() { return _id; }
+    public void set_id(int _id) { this._id = _id; }
+
+    public String get_phoneNum() { return _phoneNum; }
+    public void set_phoneNum(String _phoneNum) {this._phoneNum = _phoneNum;    }
+
+    public String get_name() { return _name; }
+    public void set_name(String _name) { this._name = _name; }
+}
