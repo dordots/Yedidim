8eb88f8906e322e88f7421a8b83f8d88852b36cd
Yossi RIzgan <yosriz@gmail.com>
2017-09-28 22:56:08 +0300

    SMS authentication
    * initial AuthEntity interface

---
+++ app/src/main/java/com/startach/yedidim/entities/AuthEntity.kt
@@ -0,0 +1,21 @@
+package com.startach.yedidim.entities
+
+import io.reactivex.Single
+
+interface AuthEntity {
+
+    fun isAuthenticated(): Single<Boolean>
+
+    /**
+     * Verify phone num, in case when needed code will be sent to phone in SMS
+     * and `AuthState.CodeSent` will be emitted, in this case use `AuthEntity.loginWithCode` to continue
+     * @param
+     */
+    fun verifyPhoneNumber(phoneNum: String): Single<AuthState>
+
+    fun loginWithCode(code: String) : Single<AuthState>
+}
+
+enum class AuthState {
+    Success, CodeSent, Failure, UnknownUser
+}
