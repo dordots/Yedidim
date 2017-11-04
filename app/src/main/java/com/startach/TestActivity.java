a6112d630e77486f79c1b9b243f6430c2afc33db
Yossi RIzgan <yosriz@gmail.com>
2017-11-04 22:23:58 +0200

    add stg flavour, add TestActivity

---
+++ app/src/main/java/com/startach/TestActivity.java
@@ -0,0 +1,78 @@
+package com.startach;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import com.startach.yedidim.R;
+import com.startach.yedidim.entities.authentication.AuthEntity;
+import com.startach.yedidim.entities.authentication.AuthState;
+import com.startach.yedidim.entities.notification.NotificationDeviceIdSyncer;
+import com.startach.yedidim.modules.App;
+import com.startach.yedidim.modules.auth.AuthModule;
+import com.startach.yedidim.modules.testactivity.TestModule;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import retrofit2.Retrofit;
+import timber.log.Timber;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Inject Retrofit retrofit;
+    @Inject AuthEntity authEntity;
+    @Inject NotificationDeviceIdSyncer deviceIdSyncer;
+    @BindView(R.id.editText2) EditText codeEditText;
+    @BindView(R.id.editTextPhone) EditText editTextPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        ButterKnife.bind(this);
+
+        ((App) getApplication()).getComponent().newTestActivitySubComponent(new TestModule(this), new AuthModule())
+                .inject(this);
+    }
+
+    @OnClick(R.id.button2)
+    public void sendSmsClicked() {
+        authEntity.isAuthenticated()
+                .flatMap(isAuthenticated -> {
+                    Timber.d("isAuthenticated = " + isAuthenticated);
+                    if (isAuthenticated) {
+                        return Single.just(AuthState.Success);
+                    } else {
+                        return authEntity.verifyPhoneNumber(editTextPhone.getText().toString())
+                                .observeOn(AndroidSchedulers.mainThread());
+                    }
+                })
+                .subscribe(authState -> {
+                    Timber.d("sendSmsClicked autState = " + authState);
+                });
+    }
+
+    @OnClick(R.id.button3)
+    public void verifyClicked() {
+        authEntity.loginWithCode(codeEditText.getText().toString())
+                .subscribe(authState -> {
+                    Timber.d("verifyClicked autState = " + authState);
+                });
+    }
+
+    @OnClick(R.id.buttonLogOut)
+    public void logout() {
+        authEntity.logout();
+    }
+
+    @OnClick(R.id.btnTest)
+    public void btnTest() {
+
+    }
+
+}
