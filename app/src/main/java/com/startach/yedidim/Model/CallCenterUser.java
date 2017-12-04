bb791810cedc973662114491550d766212b3e71d
Yossi Rizgan <yosriz@gmail.com>
2017-12-04 22:13:28 +0200

    cleaning - remove unused classes

--- app/src/main/java/com/startach/yedidim/Model/CallCenterUser.java
+++
@@ -1,9 +0,0 @@
-package com.startach.yedidim.Model;
-
-public class CallCenterUser extends User{
-    private int isActive;
-
-    public int getIsActive() { return isActive; }
-    public void setIsActive(int isActive) { this.isActive = isActive; }
-
-}
--- app/src/main/java/com/startach/yedidim/Model/VolunteerUser.java
+++
@@ -1,8 +0,0 @@
-package com.startach.yedidim.Model;
-
-public class VolunteerUser extends User{
-    private int isActive;
-
-    public int getIsActive() { return isActive; }
-    public void setIsActive(int isActive) { this.isActive = isActive; }
-}
