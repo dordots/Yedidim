9a58189863a924917d923b10020ac7e5fcf729c2
yb34982 <yaniv.bouhadana@citi.com>
2018-01-08 10:37:17 +0200

    add about us text (HTML)

--- app/src/main/java/com/startach/yedidim/MainPageFragments/AboutUsFragment.java
+++ app/src/main/java/com/startach/yedidim/MainPageFragments/AboutUsFragment.java
@@ -1,11 +1,13 @@
 package com.startach.yedidim.MainPageFragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.startach.yedidim.R;
 
@@ -24,7 +26,11 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        final View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+        WebView web = (WebView)view.findViewById(R.id.about_us_content);
+        web.setBackgroundColor(Color.TRANSPARENT);
+        web.loadUrl("file:///android_asset/aboutus.html");
+        return view;
     }
 
 }
