dac3d92ea5d33bf2dd015493b04e854a4a614f21
yb34982 <yaniv.bouhadana@citi.com>
2018-01-15 16:45:33 +0200

    1.0.10 - TestFairyInit

--- app/src/debug/java/testfairy/TestFairyInit.java
+++ app/src/debug/java/testfairy/TestFairyInit.java
@@ -2,12 +2,14 @@ package testfairy;
 
 import android.app.Activity;
 
+import com.testfairy.TestFairy;
+
 /**
  * Created by yb34982 on 24/12/2017.
  */
 
 public class TestFairyInit {
     public static void start(Activity activity){
-// No TestFairy currently (maybe later)
+        TestFairy.begin(activity, "6f4c3da555567854de9cc48b9844007c2c63eeb2");
     }
 }
